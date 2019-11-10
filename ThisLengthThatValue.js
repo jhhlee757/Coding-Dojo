9fbda8a70143d2c62cca115f8b546a47ab6cfcfe
jhhlee757 <56770669+jhhlee757@users.noreply.github.com>
2019-11-10 11:28:02 -0500

    Create ThisLengthThatValue.js

---
+++ ThisLengthThatValue.js
@@ -0,0 +1,13 @@
+var arr = []
+function newarr(num1, num2) {
+if (num1 == num2) {
+  console.log("Jinx!");
+  }
+  else {
+  for (var i = 0; i < num1.length; i++){
+    arr.push(num2);
+    }
+   return arr;
+ }
+}
+newarr()
