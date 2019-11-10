53cf8766f598e9912f494797c42f91670c79bc0f
jhhlee757 <56770669+jhhlee757@users.noreply.github.com>
2019-11-10 16:40:39 -0500

    Create PrintLowReturnHigh.js

---
+++ PrintLowReturnHigh.js
@@ -0,0 +1,14 @@
+function numbers(arr) {
+  var min = arr[0];
+  var max = arr[0];
+  for (var i = 1; i < arr.length; i++) {
+  if (arr[i] < min) {
+    min = arr[i];
+  {
+  if (arr[i] > max0 {
+    max = arr[i];
+  }
+ }
+console.log(min);
+return(max);
+}
