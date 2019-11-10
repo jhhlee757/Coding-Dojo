0a2994277265186ac2689bae23e8ac51392f70a2
jhhlee757 <56770669+jhhlee757@users.noreply.github.com>
2019-11-10 12:05:42 -0500

    Create FitTheFirstValue.js

---
+++ FitTheFirstValue.js
@@ -0,0 +1,14 @@
+function value(arr) {
+if (arr[0] > arr.length) {
+  console.log("Too big!);
+  }
+  else if {
+  (arr[0] < arr.length) {
+  console.log("Too small!");
+  }
+  else {
+  console.log("Just right!");
+  }
+ }
+}
+value(arr);
